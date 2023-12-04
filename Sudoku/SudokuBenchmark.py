514591110c9665fdad078138cf48e69b7b90a792
Vansh <vanshjjw@gmail.com>
2023-12-04 04:17:41 +0000

    reformat benchmarking

--- Sudoku/SudokuBenchmark.py
+++ Sudoku/SudokuBenchmark.py
@@ -10,27 +10,23 @@ def stringToBoard(puzzle, boardSize):
     return board
 
 
-# open sample file as string
-with open('sudoku_samples.txt', 'r') as f:
-    samples = f.read().splitlines()
+def benchmarkSamples():
+    with open('sudoku_samples.txt', 'r') as f:
+        samples = f.read().splitlines()
 
-    # First 9 samples are easy
-    for i in range(18, 20):
-        initialBoard, numberOfSolutions, *solution = samples[i].split(':')
-        boardSize = int(len(initialBoard) ** 0.5)
-        Board = stringToBoard(initialBoard, boardSize)
-        NumberOfClues = np.count_nonzero(Board)
+        for i in range(0, 10):
+            initialBoard, numberOfSolutions, *solution = samples[i].split(':')
+            boardSize = int(len(initialBoard) ** 0.5)
+            Board = stringToBoard(initialBoard, boardSize)
+            numberOfClues = np.count_nonzero(Board)
 
-        print("Benchmarking:", initialBoard, " expecting ", numberOfSolutions, " solutions",
-              " given clues ", NumberOfClues, end="\n")
+            print("Benchmarking:",initialBoard," with ",numberOfClues," clues, "," expecting ",numberOfSolutions, " solutions.")
 
-        BenchmarkParams = {'numOfNodes': 0, "Time": 0, "showEachStep": False, "ShowProgress": time.time()}
-        start = time.time()
-        SudokuSolver.sudokuSolver(Board, True, BenchmarkParams)
-        end = time.time()
-        BenchmarkParams["Time"] = end - start
+            BenchmarkParams = {'numOfNodes': 0, "Time": 0, "showEachStep": False, "ShowProgress": time.time()}
+            start = time.time()
+            SudokuSolver.sudokuSolver(Board, True, BenchmarkParams)
+            end = time.time()
+            BenchmarkParams["Time"] = end - start
 
-        print("Number of Nodes: ", BenchmarkParams['numOfNodes'], " Time: ", BenchmarkParams["Time"], end="\n\n")
-
-
+            print("Number of Nodes: ", BenchmarkParams['numOfNodes'], " Time: ", BenchmarkParams["Time"], end="\n\n")
 
