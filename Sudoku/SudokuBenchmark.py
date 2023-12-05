13c60593ca102ff20ba543e6d39ea60a4ada7128
Vansh <vanshjjw@gmail.com>
2023-12-05 14:39:15 +0000

    benchmarking with clues results

--- Sudoku/SudokuBenchmark.py
+++ Sudoku/SudokuBenchmark.py
@@ -1,6 +1,11 @@
+import copy
+import random
 import time
 import numpy as np
 import SudokuSolver
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 def stringToBoard(puzzle, boardSize):
     board = np.zeros((boardSize, boardSize))
@@ -30,3 +35,56 @@ def benchmarkSamples():
 
             print("Number of Nodes: ", BenchmarkParams['numOfNodes'], " Time: ", BenchmarkParams["Time"], end="\n\n")
 
+
+def benchmarkSpeedWithNumberOfClues():
+    with open('sudoku_samples.txt', 'r') as f:
+        samples = f.read().splitlines()
+
+    results = open("Benchmark results/results.txt", "w")
+    for i in range(0, 9):
+        sample = samples[i]
+        initialBoard, numberOfSolutions, *solution = sample.split(':')
+        boardSize = int(len(initialBoard) ** 0.5)
+        Board = stringToBoard(initialBoard, boardSize)
+
+        print("Initial Board: ", initialBoard, end="\n\n")
+        results.write("Initial Board: " + initialBoard + "\n\n")
+        numberOfClues = np.count_nonzero(Board)
+        plt.figure(i)
+
+        Times = []
+        clues = []
+        while numberOfClues > 20:
+            print("Number of Clues: ", numberOfClues, end="...")
+            results.write("Number of Clues: " + str(numberOfClues) + "...")
+            BenchmarkParams = {'numOfNodes': 0, "showEachStep": False, "ShowProgress": False}
+
+            start = time.time()
+            SudokuSolver.sudokuSolver(copy.deepcopy(Board), True, BenchmarkParams)
+            end = time.time()
+            Times.append(end - start)
+            clues.append(numberOfClues)
+            BenchmarkParams["Time"] = Times[-1]
+
+            print("Number of Nodes: ", BenchmarkParams['numOfNodes'], " Time: ", BenchmarkParams["Time"],
+                  end="\n\n")
+            results.write("Number of Nodes: " + str(BenchmarkParams['numOfNodes']) + " Time: " + str(BenchmarkParams["Time"]) + "\n")
+
+            removeRandomCLue(Board)
+            numberOfClues = np.count_nonzero(Board)
+
+        plt.plot(clues, Times, 'ro')
+        plt.ylabel('Time')
+        plt.xlabel('Number of Clues')
+        plt.savefig('SpeedVsNumberOfClues ' + str(i) + '.png')
+
+    results.close()
+
+def removeRandomCLue(Board):
+    nonZeroElementIndices = np.nonzero(Board)
+    choice = np.random.randint(0, len(nonZeroElementIndices[0]))
+    Board[nonZeroElementIndices[0][choice]][nonZeroElementIndices[1][choice]] = 0
+
+
+# benchmarkSpeedWithNumberOfClues()
+benchmarkSamples()
